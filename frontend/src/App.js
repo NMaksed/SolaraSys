5a1b768f4b76ceba872dba4c5af96b519d182a8e
Frog <omarrenardo33801@gmail.com>
2023-09-12 21:00:21 -0300

    Adicionando botões para seleção de cadastros

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,45 @@
+import React, { useState } from 'react';
 import './App.css';
-import Navbar from './pages/Pessoa/Cadastro/Appbar'
-import CadastroPessoa from './pages/Pessoa/Cadastro';
-import Header from './components/header';
-import Login from './pages/Main/login';
+import Navbar from './pages/Pessoa/Cadastro/Appbar';
+import PessoaCadastro from './pages/Pessoa/Cadastro';
+import EmpresaCadastro from './pages/Empresa/Cadastro';
 import FuncionarioCadastro from './pages/Funcionario/Cadastro';
 
 function App() {
+  const [activeComponent, setActiveComponent] = useState(null);
+
+  const handleComponentChange = (componentName) => {
+    setActiveComponent(componentName);
+  };
+
+  const renderActiveComponent = () => {
+    switch (activeComponent) {
+      case 'PessoaCadastro':
+        return <PessoaCadastro />;
+      case 'EmpresaCadastro':
+        return <EmpresaCadastro />;
+      case 'FuncionarioCadastro':
+        return <FuncionarioCadastro />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="App">
-      <Navbar/>
-      <FuncionarioCadastro/>
+      <Navbar texto="Cadastro de Entidades" />
+      <div className="button-container">
+        <button onClick={() => handleComponentChange('PessoaCadastro')}>
+          Cadastrar Pessoa
+        </button>
+        <button onClick={() => handleComponentChange('EmpresaCadastro')}>
+          Cadastrar Empresa
+        </button>
+        <button onClick={() => handleComponentChange('FuncionarioCadastro')}>
+          Cadastrar Funcionário
+        </button>
+      </div>
+      {renderActiveComponent()}
     </div>
   );
 }
