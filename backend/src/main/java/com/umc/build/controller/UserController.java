1e6837838994e3f6ba6cd01ec8cc3004712ebe72
NMaksed <nathangfonseca@hotmail.com>
2023-09-20 23:17:26 -0300

    login backend

--- backend/src/main/java/com/umc/build/controller/UserController.java
+++ backend/src/main/java/com/umc/build/controller/UserController.java
@@ -4,6 +4,7 @@ package com.umc.build.controller;
 import com.umc.build.model.User;
 import com.umc.build.serviceImpl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,4 +28,14 @@ public class UserController{
     @GetMapping("getUsuario")
     public List<User> getAllUsuarios() {return userService.getUsuario(); }
 
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        try {
+            userService.validatorUsuario(user.getEmail(), user.getSenha());
+            return ResponseEntity.ok("Usuario logado!");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao passar usuario: " + e.getMessage());
+        }
+    }
 }
