fdc335501af3b71815938e48bb6db71b0f5fa795
Nathan Fonseca <nathangfonseca@hotmail.com>
2023-10-07 16:18:41 -0300

    token generator para login

--- backend/src/main/java/com/umc/build/serviceImpl/UserServiceImpl.java
+++ backend/src/main/java/com/umc/build/serviceImpl/UserServiceImpl.java
@@ -4,10 +4,19 @@ import com.umc.build.model.Funcionario;
 import com.umc.build.model.User;
 import com.umc.build.repository.FuncionarioRepository;
 import com.umc.build.repository.UserRepository;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
+import java.security.Key;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,5 +50,32 @@ public class UserServiceImpl {
         if (usuarioOptional.isEmpty()) {
             throw new Exception("Senha ou Email Incorreto!!");
         }
+    }
+
+    /*
+    * Gera um token para autorização de login
+     */
+    public byte[] geradorToken(User user) {
+        Key key = generateHs256();
+
+        JwtBuilder token = Jwts.builder()
+                .setSubject(user.getEmail())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 5 * 60 * 1000))
+                .signWith(key, SignatureAlgorithm.HS256);
+
+        return token.compact().getBytes();
+    }
+
+    /*
+    * Cria uma chave do tipo HS256
+     */
+    public Key generateHs256() {
+        int keySizeBytes = 32;
+
+        byte[] secretKeyBytes = new byte[keySizeBytes];
+        new SecureRandom().nextBytes(secretKeyBytes);
+
+        return Keys.hmacShaKeyFor(secretKeyBytes);
     }
 }
